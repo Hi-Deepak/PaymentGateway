d76915c59c5715dde99490d813d73567b5cb026a
Kirti Jain <kirti.jain@ubuy.com>
2023-06-27 12:44:34 +0530

    List of Banks done.

--- app/src/main/java/com/example/payumoneytest/MainActivity.kt
+++ app/src/main/java/com/example/payumoneytest/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.DefaultRetryPolicy
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.payumoneytest.databinding.ActivityMainBinding
+import org.json.JSONArray
 import org.json.JSONObject
-
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var payment_id : String
     lateinit var token : String
     lateinit var encrypted_cvv: String
+    lateinit var rowData : String
+
 
 
     lateinit var binding: ActivityMainBinding
@@ -25,11 +29,76 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.pay.setOnClickListener {
-                createToken()
-
+                requestForBankList()
         }
 
     }
+
+    private fun getBankList(rowData: String) {
+        val data = JSONArray(rowData)
+        val methods = data.getJSONObject(0).getJSONArray("supported_payment_methods")
+        val listOfBankInfo = mutableListOf<BankInfo>()
+         for (i in 0 until methods.length()){
+             val method = methods.getJSONObject(i)
+             val vendor = method.getString("vendor")
+             if (vendor.equals("NetBanking")){
+                 val displayName = method.getString("display_name")
+                 val sourceType = method.getString("source_type")
+                 val bankCode = method.getString("bank_code")
+                 val status = method.getString("status")
+                 val bank = BankInfo(displayName, vendor, bankCode, sourceType,  status)
+                 listOfBankInfo.add(bank)
+             }
+         }
+
+        listOfBankInfo.forEach { bank ->
+            Log.d("GetBankList bank ", bank.toString() + "\n\n")
+        }
+    }
+
+    private fun requestForBankList() {
+        val requestQuery = Volley.newRequestQueue(this)
+
+        val jsonObjectRequest =
+            object :StringRequest(Method.GET,"https://api.paymentsos.com/supported-payment-methods"
+            ,{
+                Toast.makeText(this," success ",Toast.LENGTH_SHORT).show()
+                    rowData = it.toString()
+                    Log.e("bankList success", "> $rowData")
+                    getBankList(rowData)
+                }
+            ,{
+                Toast.makeText(this," error > ${it.message}",Toast.LENGTH_SHORT).show()
+                    Log.e("bankList error", "> ${it.message}")
+            }){
+            override fun getHeaders(): MutableMap<String, String> {
+                val header = HashMap<String, String>()
+                header["app_id"] = "com.ubuy.ubuy_india_test"
+                header["public_key"] = "0da634ec-a8ef-42bc-867b-5e970f18d55a"
+                header["Content-Type"]="application/json"
+                header["api-version"] = "1.3.0"
+                header["x-payments-os-env"] = "test"
+                header["private_key"] = "39871bbc-1cea-42ee-8992-a981f471a1d0"
+                return header
+            }
+        }
+        jsonObjectRequest.setRetryPolicy(
+            DefaultRetryPolicy(
+                30000, 3,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+            )
+        )
+        requestQuery.add(jsonObjectRequest)
+    }
+
+    data class BankInfo(
+        val displayName: String,
+        val vendor: String,
+        val bankCode: String,
+        val sourceType: String,
+        val status: String
+    )
+
 
     private fun createToken(){
 
